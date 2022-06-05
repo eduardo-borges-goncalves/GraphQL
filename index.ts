8a2d4d2d68520897ba3740fd508ac3b37971dc96
Eduardo Borges <alemaobrk1@gmail.com>
2022-06-05 15:41:33 -0300

    scalar types, schema and args

--- index.ts
+++ index.ts
@@ -1,20 +1,139 @@
-import { QueryDocumentKeys } from "graphql/language/ast"
-
 const { gql, ApolloServer } = require("apollo-server")
 
-const resolvers=  {
+type UserProps = {
+  id: number, 
+  age: number, 
+  salary: number, 
+  name: string,
+  active: boolean, 
+  house_phone: string
+}
+
+const users = [
+  {
+    id: 1,
+    age: 19, 
+    name: "Paulo", 
+    salary: 2500,
+    active: true, 
+    house_phone: "62338695428"
+  },
+  {
+    id: 2,
+    age: 19, 
+    name: "Pedro", 
+    salary: 2500,
+    active: true, 
+    house_phone: "62338695428"
+  },
+  {
+    id: 3,
+    age: 19, 
+    name: "Paulo", 
+    salary: 2500,
+    active: true, 
+    house_phone: "62338695428"
+  },
+  {
+    id: 4,
+    age: 19, 
+    name: "Paulo", 
+    salary: 2500,
+    active: true, 
+    house_phone: "62338695428"
+  },
+  {
+    id: 5,
+    age: 19, 
+    name: "Paulo", 
+    salary: 2500,
+    active: true, 
+    house_phone: "62338695428"
+  },
+]
+
+const profiles = [
+  {
+    id: 1, 
+    description: "lorem ipsum lorem ipsum"
+  },
+  {
+    id: 2, 
+    description: "lorem ipsum lorem ipsum ipsum taneba"
+  },
+]
+
+const typeDefs = gql`
+  type Product {
+    id: ID
+    name: String
+    value: Float
+  }
+
+  type User {
+    age: Int
+    salary: Float
+    name: String
+    active: Boolean
+    id: ID
+    phone: String
+    profile: Profile
+  }
+
+  type Profile {
+    id: ID
+    description: String
+  }
+
+  type Query {
+    users: [User]
+    products: [Product]
+    user(id: Int): User
+    profiles: [Profile]
+  }
+`
+
+const resolvers =  {
+  User: {
+    phone (obj: UserProps) {
+      return obj.house_phone
+    },
+    profile (user: UserProps, arg: UserProps) {
+      return profiles.find(profile => profile.id === user.id)
+    }
+  }, 
   Query: {
-    hello() {
-      return "World"
+    user (_: UserProps, arg: UserProps) {
+      return users.find(user => user.id === arg.id)
+    }, 
+    users () {
+      return users
+    }, 
+    products () {
+      const products = [
+        {
+          id: 1,
+          name: "Secador de Cabelo",
+          value: 189.00
+        },
+        {
+          id: 2,
+          name: "Wash Machine",
+          value: 1899.00
+        },
+        {
+          id: 3,
+          name: "Cooktop",
+          value: 289.00
+        },
+      ]
+      return products  
+    }, 
+    profiles () {
+      return profiles
     }
   }
 }
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
 
 const server = new ApolloServer({
   typeDefs, 
@@ -22,3 +141,37 @@ const server = new ApolloServer({
 })
 
 server.listen()
+
+
+
+/*
+ * Scalar Types
+  * - Int
+  * - Float
+  * - String
+  * - Boolean
+  * - ID
+
+*/
+
+// Dúvidas: 
+
+// O que é uma query? 
+// O que é esse type antes de Query?
+// O que é gql? Porque ele usa essas `` em volta do código igual typescript?
+// Como essa aparente tipagem cai na Query dentro do resolvers?
+// Qual a natureza dessa Query? É um objeto? porque as funções são chamadas 
+// dentro dela como métodos como se fosse uma classe?
+
+
+// O que é um Schema?
+
+// Geralmente é a representação de um objeto. 
+// SDL. Schema Definition Language
+
+// Arguments
+
+// Como eu posso fazer uma consulta no graphql, como eu posso retornar um usuário 
+// Uma espécie de interceptador da query para corrigir uma propriedade passada errada, 
+// ainda não vejo como isso pode se tornar automático mas imagino que será mostrado no 
+// futuro. 
